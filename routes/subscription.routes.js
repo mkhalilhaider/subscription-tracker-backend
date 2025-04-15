1ae13b6142d393cf6a453dabd6063ce6bd9cc089
khalil haider <khalil.haidr2109@gmail.com>
2025-04-15 16:35:35 +0500

    Removed cross-env, added route files for auth, user, and subscription, and configured routes in app.js

---
+++ routes/subscription.routes.js
@@ -0,0 +1,19 @@
+import { Router } from "express";
+
+const subscriptionRouter = Router();
+
+subscriptionRouter.get('/', (req, res)=> res.send({title:"GET all susbscriptions"}));
+
+subscriptionRouter.get('/:id', (req, res)=> res.send({title:"GET susbscription details by ID"}));
+
+subscriptionRouter.post('/', (req, res)=> res.send({title:"CREATE a new susbscription"}));
+
+subscriptionRouter.put('/:id', (req, res)=> res.send({title:"UPDATE susbscription details by ID"}));
+
+subscriptionRouter.delete('/:id', (req, res)=> res.send({title:"DELETE susbscription by ID"}));
+
+subscriptionRouter.put('/:id/cancel', (req, res)=> res.send({title:"GET all user susbscriptions"}));
+
+subscriptionRouter.get('/upcoming-renewals', (req, res)=> res.send({title:"GET upcoming renewals"}));
+
+export default subscriptionRouter;
