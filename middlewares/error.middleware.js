02c021fbbd30f875645d3673a4ab8fa1569a26b6
khalil haider <khalil.haidr2109@gmail.com>
2025-04-16 11:19:17 +0500

    Added error handling middleware

---
+++ middlewares/error.middleware.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+const errorMiddleware = (err, req, res, next) => {
+  try {
+    let error = { ...err };
+    // error.message = err.message;
+    console.error(err);
+
+    //Mongoose bad Object ID
+    if (err.name == "CastError") {
+      const message = "Resource not found!";
+      error = new Error(message);
+      error.statusCode = 404;
+    }
+
+    // Mongoose Dublicate key
+    if (err.code === 11000) {
+      const message = "Duplicate Field Value Enter";
+      error = new Error(message);
+      error.statusCode = 400;
+    }
+
+    //MOngoose Validation error
+    if (err.name === "ValidationError") {
+      const message = Object.values(err.errors).map((val) => val.message);
+      error = new Error(message.join(" "));
+      error.statusCode = 400;
+    }
+
+    res.status(error.statusCode || 500).json({
+      succes: "false",
+      error: `${error.message} || Server Error`,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export default errorMiddleware;
