898c68ae04eee6ca8849997773f5afeb4a99f8b5
khalil haider <khalil.haidr2109@gmail.com>
2025-04-16 21:21:58 +0500

    add arject middleware

--- middlewares/arcjet.middleware.js
+++ middlewares/arcjet.middleware.js
@@ -1,22 +1,21 @@
 import aj from "../config/arcjet.js";
-import {ARCJET_ENV} from "../config/env.js";
+import { ARCJET_ENV } from "../config/env.js";
 
 const arcjetMiddlware = async (req, res, next) => {
   try {
-
     if (ARCJET_ENV === "development") {
-        return next();
-      }
+      return next();
+    }
 
-    const decision = await aj.protect(req, {requested:1});
+    const decision = await aj.protect(req, { requested: 1 });
 
     if (decision.isDenied()) {
+      if (decision.reason.isRateLimit())
+        return res.status(429).json({ error: "Too Many Requests" });
+      if (decision.reason.isBot())
+        return res.status(403).json({ error: "Bot Detected" });
 
-    if (decision.reason.isRateLimit()) return res.status(429).json({ error: "Too Many Requests" });
-    if (decision.reason.isBot()) return res.status(403).json({ error: "Bot Detected" });
-    
-    return res.status(403).json({ error: "Access Denied" });
-    
+      return res.status(403).json({ error: "Access Denied" });
     }
     next();
   } catch (error) {
