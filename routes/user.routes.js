37231930cf50b2588ed78b5ea936bcae04aeb14a
khalil haider <khalil.haidr2109@gmail.com>
2025-04-16 19:57:46 +0500

    Add JWT authentication middleware and protect user routes

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { getUsers, getUser } from "../controllers/user.controller.js";
+import authorize from "../middlewares/auth.middleware.js";
+
 
 const userRouter = Router();
 
 userRouter.get("/", getUsers);
 
-userRouter.get("/:id", getUser);
+userRouter.get("/:id", authorize, getUser);
 
 userRouter.post("/", (req, res) => res.send({ title: "CREATE a new user" }));
 
