08a81054c61f7a09c872dc0de0d3fc951f7e4ee6
mkhalilhaider <khalil.haider2109@gmail.com>
2025-04-22 23:56:04 +0500

    clear typo

--- config/env.js
+++ config/env.js
@@ -2,4 +2,12 @@ import { config } from "dotenv";
 
 config({ path: `.env${process.env.NODE_ENV || "development"}.local` });
 
-export const { NODE_ENV, PORT, DB_URI, JWT_SECRET, JWT_EXPIRES_IN, ARCJET_ENV, ARCJET_KEY } = process.env;
+export const {
+  NODE_ENV,
+  PORT,
+  DB_URI,
+  JWT_SECRET,
+  JWT_EXPIRES_IN,
+  ARCJET_ENV,
+  ARCJET_KEY,
+} = process.env;
